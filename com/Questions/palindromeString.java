c3b2aa8813a653c51cda8eca8ec35c2a13d8554e
kabi239 <kavyasharma239@gmail.com>
2025-02-05 02:12:28 +0530

    palindrome or not

---
+++ com/Questions/palindromeString.java
@@ -0,0 +1,23 @@
+package com.Questions;
+
+public class palindromeString {
+    public static void main(String[] args) {
+         String s ="ababaa";
+         int i=0;
+         int j=s.length()-1;
+         boolean flag = true;
+         while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                flag=false;
+            }
+            i++;
+            j--;
+         }
+         if (flag==true) {
+             System.out.println("Palindeome");
+         } else {
+            System.out.println("not a palindrome");
+         }
+
+    }
+}
