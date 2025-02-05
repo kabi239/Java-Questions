ca9afa7031f6bb549d8a065252dc4c5106d4d9a9
kabi239 <kavyasharma239@gmail.com>
2025-02-05 21:18:12 +0530

    find The Max Frequency Char In A String

---
+++ com/Questions/findTheMaxFrequencyCharInAString.java
@@ -0,0 +1,27 @@
+package com.Questions;
+
+public class findTheMaxFrequencyCharInAString {
+     public static void main(String[] args) {
+        String a = "abc def abc asdasd";
+        int l = a.length();
+        int maxFreq=0;
+        char maxChar=' ';
+        for(int i=0;i<l;i++){
+            char fixedChar=a.charAt(i);
+            int currentFreq=0;
+            for (int j = 0; j < l; j++) {
+                if(a.charAt(j)==fixedChar){
+                    currentFreq++;
+                }
+                
+            }
+            if (currentFreq>maxFreq) {
+                maxFreq=currentFreq;
+                maxChar=fixedChar;
+            }
+
+        }
+        System.out.println("char: "+ maxChar +" with max frequency "+maxFreq);
+     }
+    
+}
