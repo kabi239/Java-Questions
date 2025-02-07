ede58e51e6e847d4f7a9e9cb6d48d447c67f0e50
kabi239 <kavyasharma239@gmail.com>
2025-02-07 23:58:27 +0530

    .

---
+++ com/Questions/reverseTheOrderOfWords.java
@@ -0,0 +1,19 @@
+package com.Questions;
+
+public class reverseTheOrderOfWords {
+    public static void main(String[] args) {
+        
+        String s ="abfjhj jdfhkjjfkd"; 
+        int l= s.length();
+        String currentword="";
+        int count= 0 ;
+        for (int i = 0; i < 10; i++) {
+            if(s.charAt(i))
+        }
+        for (int i = 0; i < l; i++) {
+            if(s.charAt(i)!= ' '){
+                currentword = currentword +s.charAt(i);
+            }
+        }
+    }
+}
