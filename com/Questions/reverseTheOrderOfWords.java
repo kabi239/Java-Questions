dc1207e6cf55287ea26eea011638e68d9feaa65a
kabi239 <kavyasharma239@gmail.com>
2025-02-08 12:57:37 +0530

    reverse the order of words

--- com/Questions/reverseTheOrderOfWords.java
+++ com/Questions/reverseTheOrderOfWords.java
@@ -1,19 +1,50 @@
 package com.Questions;
 
 public class reverseTheOrderOfWords {
+    public static void reverse(String words[]) {
+        int i=0;
+        int j= words.length-1;
+        while(i<j){
+            String temp = words[i];
+            words[i]= words[j];
+            words[j]=temp;
+            i++;
+            j--;
+        }
+    }
     public static void main(String[] args) {
         
-        String s ="abfjhj jdfhkjjfkd"; 
+        String s ="kabi max"; 
         int l= s.length();
         String currentword="";
-        int count= 0 ;
-        for (int i = 0; i < 10; i++) {
-            if(s.charAt(i))
+        int count= 0;
+
+        for (int i = 0; i < l; i++) {
+            if(s.charAt(i)==' '){
+                count++;
+            }
         }
+        count+=1;
+        String words[] = new String[count];
+
+        int index=0;
         for (int i = 0; i < l; i++) {
             if(s.charAt(i)!= ' '){
                 currentword = currentword +s.charAt(i);
             }
+            else{
+                words[index]=currentword;
+                currentword="";
+                index++;
+            }
         }
+        words[index]= currentword;
+        reverse(words);
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words[i]+" ");
+        }
+
+
     }
 }
